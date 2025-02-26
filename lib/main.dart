f65010cebac01ae1afcab881eccfc0a23d1dcbf2
AprilaAditia <aymericaditia.43@gmail.com>
2025-02-26 11:18:10 +0700

    Memasukan Projek Pencatatan Keuangan

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pencatatan_keuangan/pages/main_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Menghilangkan banner debug
+      home: MainPage(),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
